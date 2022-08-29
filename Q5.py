d1eeb8809f69e8cc915747ac963d525c8a51eced
NortinD <nortgordon@gmail.com>
2022-08-30 02:41:51 +0500

    Напишите простой калькулятор, который считывает с пользовательского ввода три строки: первое число, второе число и операцию, после чего применяет операцию к введённым числам ("первое число" "операция" "второе число") и выводит результат на экран.
    Поддерживаемые операции: +, -, /, *, mod, pow, div, где
    mod — это взятие остатка от деления,
    pow — возведение в степень,
    div — целочисленное деление.
    
    Если выполняется деление и второе число равно 0, необходимо выводить строку "Деление на 0!".
    
    Обратите внимание, что на вход программе приходят вещественные числа.

--- Q5.py
+++ Q5.py
@@ -1,22 +1,29 @@
 
-a=int(input())
+a=float(input())
+c=float(input())
 b=str(input())
-c=int(input())
+
 if b==('+'):
     print (a+c)
 if b==('-'):
     print(a-c)
-
-if b==('/') and c!=0:
-    print(a/c)
-elif c==0:
-    print('Деление на 0!')
+if b==('/'): 
+    if c!=0:
+        print(a/c)
+    elif c==0:
+        print('Деление на 0!')
 if b==('*'):
     print(a*c)
 if b==('mod'):
-    print(a%c)
+    if c!=0:
+        print(a%c)
+    elif c==0:
+        print('Деление на 0!')
 if b==('pow'):
     print(a**c)
 if b==('div'):
-    print(a//c)
+    if c!=0:
+        print(a//c)
+    elif c==0:
+        print('Деление на 0!')
 
