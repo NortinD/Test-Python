f34f9c5868713ea520007bbd2f066cc3d685a6ab
NortinD <nortgordon@gmail.com>
2022-09-01 01:17:17 +0500

    Finish

--- Q13.py
+++ Q13.py
@@ -2,18 +2,16 @@ a,b,c,d=int(input()),int(input()),int(input()),int(input())
 
 print(end='\t')
 for j in range(c,d+1):
-    print(j,end='\t')    
-print()
-
-        
-        
+    #print(j,end='\t')
+    print()
+                  
 for i in range(a,b+1):
-    print(i,end='\n') 
-    
+    #print(i,end='\n')
+    print() 
      
     for j in range(c,d+1):
         print(i*j,end='\t')
-    print()
+print()
 
 
 
